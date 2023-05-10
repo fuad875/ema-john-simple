2739b713f3afce7c07854743c6ee793abf478a37
fuad875 <fuad1115279890@gmail.com>
2023-05-10 11:51:46 +0600

    newcart

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -34,12 +34,23 @@ const Shop = () => {
         // console.log('locat storage finishd');
      },[products])
 
-    const handleAddtocart = (product) =>{
+    const handleAddtocart = (selectedProduct) =>{
+        let newCart = []
+        const exists = cart.find(product => product.id ===selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart,selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest,exists];
+        }
         // console.log(product);
         // cart.push(product);
-        const newCart =[...cart,product];
+        // const newCart =[...cart,selectedProduct];
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectedProduct.id);
     }
        
     return (
